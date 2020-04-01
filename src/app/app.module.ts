8fd7921e27bb8fa05aa414d4d0c36c6eda4f46a6
OreOluwaDaniel <=>
2020-04-01 21:25:31 +0100

    tried template form and created a branvh for it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms' ;
 
 import { AppComponent } from './app.component';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule ,
-    HttpClient
+    HttpClientModule ,
+    FormsModule ,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+      } ,
+      {
+        path: 'login' ,
+        component: LoginComponent
+      } ,
+      {
+        path: '**' ,
+        component: NotfoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+email = 'hello there' ;
+username = 'Username' ;
+password ;
 
+  constructor() { }
   ngOnInit() {
+    console.log(this.email);
+  }
+
+  log(val){
+    console.log(val);
+
   }
 
 }
