60f6332ee571a66d0005756a185505c7020c7ec3
OreOluwaDaniel <=>
2020-04-03 21:38:54 +0100

    generated sign up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
@@ -14,7 +15,8 @@ import { NotfoundComponent } from './notfound/notfound.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule ,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,9 +17,9 @@ export class LoginComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.rForm = fb.group({
       // for the email and password and all quotes may or may not be added
-      email: [null, Validators.required] , // the first value null sets the initial value of the form
-     password: [null, Validators.compose([Validators.required, Validators.minLength(8)])],
-      validate: ''
+      email: [null, Validators.compose([Validators.required, Validators.pattern('banana')])] , // the first value null sets the initial value of the form
+     password: [null, Validators.compose([Validators.required, Validators.minLength(8)])]
+    //  ,validate: ''
     }) ;
   }
 
@@ -28,19 +28,21 @@ export class LoginComponent implements OnInit {
     this.email = post.email ;
   }
 
+  log(x){ console.log(x) }
+
 
   ngOnInit() {
-    this.rForm.get('validate').valueChanges.subscribe(
-      (validate) => {
-        if(validate == '1'){
-          this.rForm.get('email').setValidators([Validators.required, Validators.minLength(3)]);
-          this.titleAlert = 'Email should consist of at least 3 characters' ;
-        } else {
-          this.rForm.get('email').setValidators(Validators.required) ;
-        }
-        this.rForm.get('email').updateValueAndValidity() ;
-      }
-    ) ;
+    // this.rForm.get('validate').valueChanges.subscribe(
+    //   (validate) => {
+    //     if(validate == '1'){
+    //       this.rForm.get('email').setValidators([Validators.required, Validators.minLength(3)]);
+    //       this.titleAlert = 'Email should consist of at least 3 characters' ;
+    //     } else {
+    //       this.rForm.get('email').setValidators(Validators.required) ;
+    //     }
+    //     this.rForm.get('email').updateValueAndValidity() ;
+    //   }
+    // ) ;
   }
 
 
